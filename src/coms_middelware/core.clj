c42164aa2361c2038440f57dc61bb2c28b0d4536
iCeMan <fabiodiasfrancisco27@gmail.com>
2020-10-05 13:46:59 +0100

    Initial Commit

---
+++ src/coms_middelware/core.clj
@@ -0,0 +1,3 @@
+(ns coms-middelware.core)
+
+
---
+++ test/coms_middelware/core_test.clj
@@ -0,0 +1,7 @@
+(ns coms-middelware.core-test
+  (:require [clojure.test :refer :all]
+            [coms-middelware.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
