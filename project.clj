8f703e2b12ef267192e1a7691c00392896328024
iCeMan <fabiodiasfrancisco27@gmail.com>
2020-10-16 17:29:11 +0100

    fixed tests

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [clojure-data-grinder-core "0.1.0-SNAPSHOT"]
+                 [clojure-data-grinder-core "0.1.0"]
                  [pt.iceman/comms-common "1.0-SNAPSHOT"]]
   :repl-options {:init-ns coms-middelware.core})
